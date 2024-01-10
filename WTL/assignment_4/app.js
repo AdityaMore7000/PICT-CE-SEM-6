4623ea6e3a354ac345475f2a8b950299d28fad7a
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-01-10 04:02:54 +0000

    complete assignment 4

---
+++ WTL/assignment_4/app.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Event} e 
+ */
+function handleSubmit(e){
+    const {firstName,lastName, m,s,cs} = e.target
+    const avg = (parseInt(m.value)+parseInt(s.value) + parseInt(cs.value))/3
+    alert(`Hello ${firstName.value} ${lastName.value} your average score is ${avg}`)
+    e.preventDefault();
+
+}
+
+const form = document.querySelector('#form')
+form.addEventListener('submit',handleSubmit)
