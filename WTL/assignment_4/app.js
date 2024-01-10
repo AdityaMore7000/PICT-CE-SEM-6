27bdc299e887abd469a33d7997ebfa96025c4fee
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-01-10 04:09:24 +0000

    modified app.js

--- WTL/assignment_4/app.js
+++ WTL/assignment_4/app.js
@@ -6,7 +6,6 @@ function handleSubmit(e){
     const avg = (parseInt(m.value)+parseInt(s.value) + parseInt(cs.value))/3
     alert(`Hello ${firstName.value} ${lastName.value} your average score is ${avg}`)
     e.preventDefault();
-
 }
 
 const form = document.querySelector('#form')
