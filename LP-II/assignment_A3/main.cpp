d860fae47b724392f33ee9fb6207c56cb5d25209
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-01-16 04:26:16 +0000

    refactor

--- LP-II/assignment_A3/main.cpp
+++ LP-II/assignment_A3/main.cpp
@@ -3,16 +3,15 @@
 #define V 5
 using namespace std;
 
-
 int minKey(int key[], bool mstSet[])
 {
     // Initialize min value
     int min = INT_MAX, min_index;
- 
+
     for (int v = 0; v < V; v++)
         if (mstSet[v] == false && key[v] < min)
             min = key[v], min_index = v;
- 
+
     return min_index;
 }
 
@@ -42,12 +41,12 @@ void primMST(int graph[V][V])
         key[i] = INT_MAX, mstSet[i] = false;
     key[0] = 0;
     parent[0] = -1;
-    for (int count = 0; count < V - 1; count++) {
+    for (int count = 0; count < V - 1; count++)
+    {
         int u = minKey(key, mstSet);
         mstSet[u] = true;
         for (int v = 0; v < V; v++)
-            if (graph[u][v] && mstSet[v] == false
-                && graph[u][v] < key[v])
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
                 parent[v] = u, key[v] = graph[u][v];
     }
     printMST(parent, graph);
@@ -55,11 +54,11 @@ void primMST(int graph[V][V])
 
 void primsAlgorithm()
 {
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
+    int graph[V][V] = {{0, 2, 0, 6, 0},
+                       {2, 0, 3, 8, 5},
+                       {0, 3, 0, 0, 7},
+                       {6, 8, 0, 0, 9},
+                       {0, 5, 7, 9, 0}};
     primMST(graph);
     return;
 }
