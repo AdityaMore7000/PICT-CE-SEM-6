0b773d2e1d3320fb8f4d89a164e4dba6bf607d11
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-01-16 09:15:47 +0000

    remove comments

--- WTL/calculator/app.js
+++ WTL/calculator/app.js
@@ -1,31 +1,23 @@
-"use strict";
 
-var input = document.getElementById('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
-  operator = document.querySelectorAll('.operators div'), // operator buttons
-  result = document.getElementById('result'), // equal button
-  clear = document.getElementById('clear'), // clear button
-  resultDisplayed = false; // flag to keep an eye on what output is displayed
+var input = document.getElementById('input'),
+  number = document.querySelectorAll('.numbers div'),
+  operator = document.querySelectorAll('.operators div'),
+  result = document.getElementById('result'),
+  clear = document.getElementById('clear'),
+  resultDisplayed = false;
 
-// adding click handlers to number buttons
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function(e) {
 
-    // storing current input string and its last character in variables - used later
     var currentString = input.innerHTML;
     var lastChar = currentString[currentString.length - 1];
 
-    // if result is not diplayed, just keep adding
     if (resultDisplayed === false) {
       input.innerHTML += e.target.innerHTML;
     } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      // if result is currently displayed and user pressed an operator
-      // we need to keep on adding to the string for next operation
       resultDisplayed = false;
       input.innerHTML += e.target.innerHTML;
     } else {
-      // if result is currently displayed and user pressed a number
-      // we need clear the input string and add the new input to start the new opration
       resultDisplayed = false;
       input.innerHTML = "";
       input.innerHTML += e.target.innerHTML;
@@ -34,33 +26,24 @@ for (var i = 0; i < number.length; i++) {
   });
 }
 
-// adding click handlers to number buttons
 for (var i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
     var currentString = input.innerHTML;
     var lastChar = currentString[currentString.length - 1];
-
-    // if last character entered is an operator, replace it with the currently pressed one
-    if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
       var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
       input.innerHTML = newString;
     } else if (currentString.length == 0) {
-      // if first key pressed is an opearator, don't do anything
-      console.log("enter a number first");
+     console.log("enter a number first");
     } else {
-      // else just add the operator pressed to the input
       input.innerHTML += e.target.innerHTML;
     }
 
   });
 }
 
-// on click of 'equal' button
 result.addEventListener("click", function() {
 
-  // this is the string that we will be processing eg. -10+26+33-56*34/23
   var inputString = input.innerHTML;
 
   // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
