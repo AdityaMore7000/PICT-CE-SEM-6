509d941759664678afc3fef0d34b088e8af30241
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-01-16 04:17:44 +0000

    complete prims algo

--- LP-II/assignment_A3/main.cpp
+++ LP-II/assignment_A3/main.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 #define vi vector<int>
+#define V 5
 using namespace std;
+
+
+int minKey(int key[], bool mstSet[])
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
+ 
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] < min)
+            min = key[v], min_index = v;
+ 
+    return min_index;
+}
+
+void printMST(int parent[], int graph[V][V])
+{
+    cout << "Edge \tWeight\n";
+    for (int i = 1; i < V; i++)
+        cout << parent[i] << " - " << i << " \t"
+             << graph[i][parent[i]] << " \n";
+}
+
 void printArray(vi &a)
 {
     for (int i : a)
@@ -9,8 +32,35 @@ void printArray(vi &a)
     }
     cout << '\n';
 }
+
+void primMST(int graph[V][V])
+{
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+    for (int i = 0; i < V; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+    key[0] = 0;
+    parent[0] = -1;
+    for (int count = 0; count < V - 1; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        for (int v = 0; v < V; v++)
+            if (graph[u][v] && mstSet[v] == false
+                && graph[u][v] < key[v])
+                parent[v] = u, key[v] = graph[u][v];
+    }
+    printMST(parent, graph);
+}
+
 void primsAlgorithm()
 {
+    int graph[V][V] = { { 0, 2, 0, 6, 0 },
+                        { 2, 0, 3, 8, 5 },
+                        { 0, 3, 0, 0, 7 },
+                        { 6, 8, 0, 0, 9 },
+                        { 0, 5, 7, 9, 0 } };
+    primMST(graph);
     return;
 }
 void selectionSort()
